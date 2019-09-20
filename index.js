f183e87aed6e495bee14b86bc3cc8dd2ccdaf95b
Jeremy Thery <jeremy.thery@atos.net>
2019-09-20 17:07:41 +0200

    factory code

--- index.js
+++ index.js
@@ -4,49 +4,41 @@ var age = $("#age");
 var pseudo = $("#pseudo");
 var mdp = $("#mdp");
 var confirmMdp = $("#confirmMdp");
-
+// animate page
 $(document).ready(function () {
-    $(".container").hide().fadeIn("slow");
+    $(".container").hide().fadeIn(3000);
 })
+// function verication age and password
+age.keyup(function () {
+    if (age.val() == 0) {
+        $("#ageVerif").text("Votre champ doit être un nombre").css('color', 'red');
+    } else if (age.val() >= 18) {
+        $("#ageVerif").text('vous êtes majeur.');
+        $("#ageVerif").animate({
+            fontSize: '1.2em'
+        }, "slow").removeClass().css('color', 'green');
+    } else if (age.val() < 18) {
+        $("#ageVerif").text('vous êtes mineur.');
+        $("#ageVerif").animate({
+            fontSize: '1em'
+        }, "slow").css('color', 'red');
+    }
+});
 
-ageFunction();
-passwordFunction();
-
-function ageFunction() {
-    age.keyup(function () {
-        if (age.val() == 0) {
-            $("#ageVerif").text("Votre champ doit être un nombre").addClass('text-warning');
-        } else if (age.val() >= 18) {
-            $("#ageVerif").text('vous êtes majeur.');
-            $("#ageVerif").animate({ fontSize: '3em' }, "slow").removeClass().addClass('text-success');
-        } else if (age.val() < 18) {
-            $("#ageVerif").text('vous êtes mineur.');
-            $("#ageVerif").animate({ fontSize: '1em' }, "slow").addClass('text-warning');
-        } 
-    });
-}
-
-function passwordFunction() {
-    mdp.change(function () {
-        if (mdp.val() != confirmMdp.val()) {
-            $(".mdpVerif").text(" Les 2 mots de passes ne correspondent pas");
-        } else {
-            $(".mdpVerif").text(" ");
-        }
-    })
-    confirmMdp.change(function () {
-        if (mdp.val() != confirmMdp.val()) {
-            $(".mdpVerif").text(" Les 2 mots de passes ne correspondent pas");
-        } else {
-            $(".mdpVerif").text(" ");
-        }
-    })
-}
-
-$("#submit").click(function () {
-    if ($("input[name='civility']:checked").val() == undefined || lastname.val() == '' || firstname.val() == '' || age.val() == '' || pseudo.val() == '' || mdp.val() == '' || confirmMdp.val() == '') {
-        $("#helloText").text("Veuillez remplir tous les champs").addClass("text-warning");
+$("#mdp, #confirmMdp").change(function () {
+    if (mdp.val() != confirmMdp.val()) {
+        $(".mdpVerif").text(" Les 2 mots de passes ne correspondent pas").css('color', 'red');
     } else {
-        $("#helloText").html($("input[name='civility']:checked").val() + '<br>' + lastname.val() + '<br>' + firstname.val() + '<br>' + age.val() + '<br>' + pseudo.val() + '<br>' + mdp.val() + '<br>' + confirmMdp.val()).removeClass();
+        $(".mdpVerif").text(" ");
+    }
+})
+// display forms with button id submit
+$("#submit").click(function () {
+    if ($("input[name='civility']:checked").val() == undefined || lastname.val() == '' || firstname.val() == '' || age.val() == '' || pseudo.val() == '' || $("#mdp").val() == '' || confirmMdp.val() == '' || mdp.val() != confirmMdp.val()) {
+        $("#display-error").text("Veuillez remplir tous les champs").css('color', 'red');
+    } else {
+        $("#helloText").html($("input[name='civility']:checked").val() + '<br>' + lastname.val() + '<br>' + firstname.val() + '<br>' + age.val() + '<br>' + pseudo.val() + '<br>' + mdp.val() + '<br>' + confirmMdp.val()).hide().fadeIn(3000);
+        $("#d-none-card").hide();
+        $(".cardText").fadeIn(1500).removeClass("d-none");
     }
 })
